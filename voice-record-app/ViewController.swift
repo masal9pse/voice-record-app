45386b68853edf19d5ce3cd5dac16ac5afc18122
masal9pse <returnymgstokh@icloud.com>
2021-10-24 08:37:21 +0900

    音声の登録、再生に成功

--- voice-record-app/ViewController.swift
+++ voice-record-app/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController,AVAudioRecorderDelegate,AVAudioPlayerDelegate {
+//class ViewController: UIViewController,AVAudioRecorderDelegate,AVAudioPlayerDelegate {
+class ViewController: UIViewController,AVAudioPlayerDelegate {
     @IBOutlet weak var recordBTN: UIButton!
     @IBOutlet weak var playBTN: UIButton!
     var soundRecorder: AVAudioRecorder!
@@ -16,10 +17,8 @@ class ViewController: UIViewController,AVAudioRecorderDelegate,AVAudioPlayerDele
     var fileName = "audioFile.m4a"
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    @IBAction func recordAct(_ sender: Any) {}
-    @IBAction func playAct(_ sender: Any) {
+        setupRecoder()
+        playBTN.isEnabled = false
     }
     
     func getDocumentsDirector() -> URL {
@@ -43,6 +42,52 @@ class ViewController: UIViewController,AVAudioRecorderDelegate,AVAudioPlayerDele
             soundRecorder.prepareToRecord()
         }catch {
             print(error)
+        }
+    }
+    
+    func setupPlayer(){
+        let audioFilename = getDocumentsDirector().appendingPathComponent(fileName)
+        do{
+            soundPlayer = try AVAudioPlayer(contentsOf: audioFilename)
+            soundPlayer.delegate = self
+            soundPlayer.prepareToPlay()
+            soundPlayer.volume = 1.0
+        } catch {
+          print(error)
+        }
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        playBTN.isEnabled = true
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        recordBTN.isEnabled = true
+        playBTN.setTitle("play", for: .normal)
+    }
+    
+    @IBAction func recordAct(_ sender: Any) {
+        if recordBTN.titleLabel?.text == "Record" {
+            soundRecorder.record()
+            recordBTN.setTitle("Stop", for: .normal)
+            playBTN.isEnabled = false
+        } else {
+            soundRecorder.stop()
+            recordBTN.setTitle("Record", for: .normal)
+            playBTN.isEnabled = true
+        }
+    }
+    
+    @IBAction func playAct(_ sender: Any) {
+        if playBTN.titleLabel?.text == "Play" {
+            playBTN.setTitle("Stop", for: .normal)
+            recordBTN.isEnabled = false
+            setupPlayer()
+            soundPlayer.play()
+        } else {
+            soundPlayer.stop()
+            playBTN.setTitle("Play", for: .normal)
+            recordBTN.isEnabled = false
         }
     }
 }
