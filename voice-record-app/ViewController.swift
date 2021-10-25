66030e3c9de35c03993cb83d698c6fa175d24015
masal9pse <returnymgstokh@icloud.com>
2021-10-26 01:59:30 +0900

    一覧表示の実装、deletegaはわからん。

--- voice-record-app/ViewController.swift
+++ voice-record-app/ViewController.swift
@@ -10,15 +10,45 @@ import AVFoundation
 
 //class ViewController: UIViewController,AVAudioRecorderDelegate,AVAudioPlayerDelegate {
 //class ViewController: UIViewController,AVAudioPlayerDelegate {
-class ViewController: UIViewController {
+//class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+//    class ViewController: UIViewController{
+    let TODO = ["牛乳を買う", "掃除をする", "アプリ開発の勉強をする"] //追加②
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TODO.count
+    }
+
+    //追加④ セルに値を設定するデータソースメソッド（必須）
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セルを取得する
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        // セルに表示する値を設定する
+        cell.textLabel!.text = TODO[indexPath.row]
+        return cell
+    }
+
+    // delegate => tableViewで例える
+    // 必須のメソッド、numberOfRowsInSectionやcellForRowAtなどがあるがこれを定義しておけば
+    // UITableViewDelegate,UITableViewDataSourceを宣言する必要もないし、
+    // tableView.dataSource = selfする必要もないし、そもそもこれがなくても動作する
+    // まだdelegateの存在意味をわかっていない。 =>
+    // 一覧表示で実装してみたが、tableView.dataSource = self
+    // tableView.delegate = selfこの２つをコメントアウトするとtableViewに定義したTodoリストが表示されなくなった。
+    // => これによりdeletegaはphpでいうインターフェースと同じものだと思っていたがこれで違うものだとわかった。、
+    // しかし、AVAudioRecorderDelegate,AVAudioPlayerDelegateでは宣言しなくても動く。
+    // だだAVAudioRecorderDelegate,AVAudioPlayerDelegateはviewDidLoadでtableView.dataSource = selfのような委託処理をやっていない。
+    // => delegateわからん。
     @IBOutlet weak var recordBTN: UIButton!
     var soundRecorder: AVAudioRecorder!
     var fileName = "record.m4a"
     override func viewDidLoad() {
         super.viewDidLoad()
 //        setupRecoder()
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
+    @IBOutlet weak var tableView: UITableView!
     func getDocumentsDirector() -> URL {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return paths[0]
@@ -51,11 +81,6 @@ class ViewController: UIViewController {
             print(error)
         }
     }
-    
-    
-//    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
-//        playBTN.isEnabled = true
-//    }
     
     func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
         recordBTN.isEnabled = true
